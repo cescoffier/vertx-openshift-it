374395abbccb7e7482aae6ec7c3c77455e8ef21d
Clement Escoffier <clement.escoffier@gmail.com>
2016-12-13 08:36:43 +0100

    Add message to assertions in embedded-http-it

--- embedded-http-it/src/test/java/io/vertx/openshift/embedded/EmbeddedServerIT.java
+++ embedded-http-it/src/test/java/io/vertx/openshift/embedded/EmbeddedServerIT.java
@@ -7,17 +7,18 @@ import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 import static com.jayway.awaitility.Awaitility.await;
 import static io.fabric8.kubernetes.assertions.Assertions.assertThat;
 import static io.restassured.RestAssured.get;
+import static org.assertj.core.api.Fail.fail;
 import static org.hamcrest.Matchers.containsString;
 
 /**
@@ -27,83 +28,106 @@ import static org.hamcrest.Matchers.containsString;
 @RunAsClient
 public class EmbeddedServerIT {
 
-    private static final String NAME = "embedded-http";
+  private static final String NAME = "embedded-http";
 
-    @ArquillianResource
-    private KubernetesClient client;
+  @ArquillianResource
+  private KubernetesClient client;
 
-    private Route route;
-    private OpenShiftClient oc;
+  private Route route;
+  private OpenShiftClient oc;
 
-    @Before
-    public void initialize() {
-        oc = client.adapt(OpenShiftClient.class);
-        // The route is exposed using .vagrant.f8 suffix, delegate to openshift to
-        // get a public URL
-        oc.routes()
-            .withName(NAME).delete();
+  @Before
+  public void initialize() {
+    oc = client.adapt(OpenShiftClient.class);
+    // The route is exposed using .vagrant.f8 suffix, delegate to openshift to
+    // get a public URL
+    oc.routes()
+      .withName(NAME).delete();
 
-        Route route = oc.routes().createNew()
-            .withNewMetadata().withName(NAME).endMetadata()
-            .withNewSpec()
-            .withNewTo().withName(NAME).withKind("Service").endTo()
-            .endSpec()
-            .done();
+    Route route = oc.routes().createNew()
+      .withNewMetadata().withName(NAME).endMetadata()
+      .withNewSpec()
+      .withNewTo().withName(NAME).withKind("Service").endTo()
+      .endSpec()
+      .done();
 
-        assertThat(route).isNotNull();
-        this.route = route;
+    ensureThat("the route is exposed", () -> assertThat(route).isNotNull());
+    this.route = route;
+  }
+
+  private <T> T ensureThat(String msg, Callable<T> callable) {
+    try {
+      return callable.call();
+    } catch (Throwable t) {
+      fail("Fail ensuring '" + msg + "'", t);
+      return null;
     }
+  }
 
-    @Test
-    public void testAppProvisionsRunningPods() throws Exception {
-        assertThat(client).deployments().pods().isPodReadyForPeriod();
+  private void ensureThat(String msg, Runnable runnable) {
+    try {
+      runnable.run();
+    } catch (Throwable t) {
+      fail("Fail ensuring '" + msg + "'", t);
     }
+  }
 
-    @Test
-    public void testInvokingTheService() {
-        await().atMost(1, TimeUnit.MINUTES).until(this::isServed);
+  @Test
+  public void testAppProvisionsRunningPods() throws Exception {
+    ensureThat("the pod is running for 10 seconds", () ->
+      assertThat(client).deployments().pods().isPodReadyForPeriod());
+  }
 
-        get(url()).then().assertThat()
-            .statusCode(200)
-            .body(containsString("Hello World!"));
+  @Test
+  public void testInvokingTheService() {
+    ensureThat("the route is accessible",
+      () -> await().atMost(1, TimeUnit.MINUTES).until(this::isServed));
+
+    ensureThat("the route is served correctly", () ->
+      get(url()).then().assertThat()
+        .statusCode(200)
+        .body(containsString("Hello World!")));
+  }
+
+  @Test
+  public void testWithTwoReplicas() {
+    oc.deploymentConfigs().withName(NAME)
+      .edit().editSpec().withReplicas(2).endSpec().done();
+
+    ensureThat("the route is accessible",
+      () -> await().atMost(1, TimeUnit.MINUTES).until(this::isServed));
+
+    ensureThat("the route can be called several times in a raw", () -> {
+      get(url()).then().assertThat().statusCode(200)
+        .body(containsString("Hello World!"));
+      get(url()).then().assertThat().statusCode(200)
+        .body(containsString("Hello World!"));
+      get(url()).then().assertThat().statusCode(200)
+        .body(containsString("Hello World!"));
+      get(url()).then().assertThat().statusCode(200)
+        .body(containsString("Hello World!"));
+    });
+
+    oc.deploymentConfigs().withName(NAME)
+      .edit().editSpec().withReplicas(1).endSpec().done();
+  }
+
+
+  private URL url() {
+    try {
+      return new URL("http://" + route.getSpec().getHost());
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    @Test
-    public void testWithTwoReplicas() {
-        oc.deploymentConfigs().withName(NAME)
-            .edit().editSpec().withReplicas(2).endSpec().done();
-
-        await().atMost(1, TimeUnit.MINUTES).until(this::isServed);
-
-        get(url()).then().assertThat().statusCode(200)
-            .body(containsString("Hello World!"));
-        get(url()).then().assertThat().statusCode(200)
-            .body(containsString("Hello World!"));
-        get(url()).then().assertThat().statusCode(200)
-            .body(containsString("Hello World!"));
-        get(url()).then().assertThat().statusCode(200)
-            .body(containsString("Hello World!"));
-
-        oc.deploymentConfigs().withName(NAME)
-            .edit().editSpec().withReplicas(1).endSpec().done();
+  private boolean isServed() {
+    try {
+      return get(url()).getStatusCode() == 200;
+    } catch (Exception e) {
+      return false;
     }
-
-
-    private URL url() {
-        try {
-            return new URL("http://" + route.getSpec().getHost());
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private boolean isServed() {
-        try {
-            return get(url()).getStatusCode() == 200;
-        } catch (Exception e) {
-            return false;
-        }
-    }
+  }
 
 
 }
