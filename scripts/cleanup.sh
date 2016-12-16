fafcdf63568cc30d454f675adb82a79cfb34376e
Clement Escoffier <clement.escoffier@gmail.com>
2016-12-16 14:20:44 +0100

    add a script cleaning up a project

---
+++ scripts/cleanup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+PROJECT=$(oc project -q)
+echo "Cleaning up current project: ${PROJECT}"
+oc delete routes --all
+oc delete services --all
+oc delete rc --all
+oc delete dc --all
+oc delete pods --all
+oc delete bc --all
+oc delete is --all
