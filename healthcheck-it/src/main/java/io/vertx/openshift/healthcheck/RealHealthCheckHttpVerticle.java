74fd92a1465c3d362cc80775d2a068497ed958e4
Martin Spisiak <mspisiak@redhat.com>
2017-10-31 13:55:14 +0100

    Package naming refactoring Added circuit breaker IT module to parent module Updated some dependency versions

---
+++ healthcheck-it/src/main/java/io/vertx/openshift/healthcheck/RealHealthCheckHttpVerticle.java
@@ -0,0 +1,56 @@
+package io.vertx.openshift.healthcheck;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.ext.healthchecks.HealthCheckHandler;
+import io.vertx.ext.healthchecks.HealthChecks;
+import io.vertx.ext.web.Router;
+
+/**
+ * @author <a href="http://escoffier.me">Clement Escoffier</a>
+ * @author Slavomir Krupa (slavomir.krupa@gmail.com)
+ */
+public class RealHealthCheckHttpVerticle extends AbstractVerticle {
+
+  public static final String KILL = "kill";
+
+  @Override
+  public void start(Future<Void> future) throws Exception {
+    Router internalRouter = Router.router(vertx);
+
+    HealthChecks globalHealthChecks = HealthChecks.create(vertx);
+
+    final HealthCheckHandler globalHealthChecksHandler = HealthCheckHandler.createWithHealthChecks(globalHealthChecks);
+    internalRouter.get("/isAlive").handler(globalHealthChecksHandler);
+    internalRouter.get("/start").handler(globalHealthChecksHandler);
+
+    vertx.eventBus().consumer(KILL,
+      m -> globalHealthChecks.register(
+        m.body().toString(),
+        f -> f.fail(m.body().toString())
+      )
+    );
+    vertx.eventBus().consumer(HealthCheckHttpVerticle.REAL_CHECKS_TIMEOUT,
+      m -> globalHealthChecks.register(HealthCheckHttpVerticle.REAL_CHECKS_TIMEOUT,
+        f -> {
+        })
+    );
+
+    vertx.eventBus().consumer(HealthCheckHttpVerticle.REAL_CHECKS_THROW_EXCEPTION,
+      m -> globalHealthChecks.register(HealthCheckHttpVerticle.REAL_CHECKS_THROW_EXCEPTION,
+        f -> {
+          throw new IllegalArgumentException();
+        })
+    );
+
+    vertx.createHttpServer()
+      .requestHandler(internalRouter::accept)
+      .listen(8088, handler -> {
+        if (handler.succeeded()) {
+          future.complete();
+        } else {
+          future.fail(handler.cause());
+        }
+      });
+  }
+}
---
+++ jdbc-it/src/main/java/io/vertx/openshift/jdbc/DDLTest.java
@@ -0,0 +1,63 @@
+package io.vertx.openshift.jdbc;
+
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonArray;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
+import io.vertx.ext.web.RoutingContext;
+
+import static io.vertx.openshift.jdbc.TestUtil.*;
+
+/**
+ * @author Thomas Segismont
+ */
+public class DDLTest implements Handler<RoutingContext> {
+
+  private final JDBCClient jdbcClient;
+
+  public DDLTest(JDBCClient jdbcClient) {
+    this.jdbcClient = jdbcClient;
+  }
+
+  public String getPath() {
+    return "/ddl";
+  }
+
+  @Override
+  public void handle(RoutingContext rc) {
+    jdbcClient.getConnection(ar -> {
+      if (ar.failed()) {
+        fail(rc, ar.cause());
+        return;
+      }
+
+      SQLConnection connection = ar.result();
+      rc.response().bodyEndHandler(v -> {
+        connection.close();
+      });
+
+      String sql = "alter table definition add constraint name_unique unique (name)";
+      connection.execute(sql, execute -> {
+        if (execute.failed()) {
+          fail(rc, execute.cause());
+          return;
+        }
+
+        connection.updateWithParams("insert into definition (name) values (?)", new JsonArray().add("def"), ires -> {
+          if (ires.failed()) {
+            fail(rc, ires.cause());
+            return;
+          }
+
+          connection.updateWithParams("insert into definition (name) values (?)", new JsonArray().add("def"), ires2 -> {
+            if (ires2.succeeded()) {
+              fail(rc, "Unique constraint was not applied");
+            } else {
+              rc.response().setStatusCode(200).end();
+            }
+          });
+        });
+      });
+    });
+  }
+}
