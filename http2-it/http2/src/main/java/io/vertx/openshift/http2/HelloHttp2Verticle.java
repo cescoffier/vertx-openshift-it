407729d5932f334fff683b423725f029859fb8b9
Martin Spisiak <mspisiak@redhat.com>
2018-04-19 16:47:46 +0200

    Added new test cases for gRPC Refactored http2-it module - it now contains 2 submodules, one for grpc server and one for all the other stuff

---
+++ http2-it/http2/src/main/java/io/vertx/openshift/http2/HelloHttp2Verticle.java
@@ -0,0 +1,35 @@
+package io.vertx.openshift.http2;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.net.PemKeyCertOptions;
+
+/**
+ * @author <a href="http://escoffier.me">Clement Escoffier</a>
+ */
+public class HelloHttp2Verticle extends AbstractVerticle {
+
+  @Override
+  public void start() {
+    vertx.exceptionHandler(Throwable::printStackTrace);
+
+
+    HttpServer server =
+      vertx.createHttpServer(new HttpServerOptions()
+        .setUseAlpn(true)
+        .setSsl(true)
+        .setPemKeyCertOptions(
+          new PemKeyCertOptions().setKeyPath("server-key.pem").setCertPath("server-cert.pem")
+        )
+      );
+
+    server.requestHandler(req -> {
+      System.out.println("Handling request on Aloha " + req.version());
+      req.response().putHeader("content-type", "text/html").end("<html><body>" +
+        "<h1>Aloha from vert.x!</h1>" +
+        "<p>version = " + req.version() + "</p>" +
+        "</body></html>");
+    }).listen(8081);
+  }
+}
