af261f6e09db8c6ecc2b68320048caa97a523cbb
Martin Spisiak <mspisiak@redhat.com>
2018-05-10 15:15:49 +0200

    Removed redundant license statements Removed unused imports Code optimization

--- sso-auth/src/main/java/io/vertx/openshift/auth/RestApplication.java
+++ sso-auth/src/main/java/io/vertx/openshift/auth/RestApplication.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.vertx.openshift.auth;
 
 import io.vertx.core.AbstractVerticle;
@@ -21,8 +5,6 @@ import io.vertx.core.Future;
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpVersion;
 import io.vertx.core.json.JsonObject;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.auth.PubSecKeyOptions;
 import io.vertx.ext.auth.jwt.JWTAuth;
 import io.vertx.ext.auth.jwt.JWTAuthOptions;
@@ -31,7 +13,6 @@ import io.vertx.ext.auth.oauth2.providers.KeycloakAuth;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.JWTAuthHandler;
-import io.vertx.ext.web.handler.OAuth2AuthHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.openshift.auth.service.Greeting;
 
@@ -43,7 +24,6 @@ public class RestApplication extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> done) {
-    // Create a router object.
     Router router = Router.router(vertx);
     router.get("/health").handler(rc -> rc.response().end("OK"));
 
@@ -119,10 +99,8 @@ public class RestApplication extends AbstractVerticle {
         .addPubSecKey(new PubSecKeyOptions()
           .setAlgorithm("RS256")
           .setPublicKey(System.getenv("REALM_PUBLIC_KEY")))
-        // since we're consuming keycloak JWTs we need to locate the permission claims in the token
         .setPermissionsClaimKey("realm_access/roles"))));
 
-    // This is how one can do RBAC, e.g.: only admin is allowed
     router.get("/jwt-greeting").handler(ctx ->
       ctx.user().isAuthorized("booster-admin", authz -> {
         if (authz.succeeded() && authz.result()) {
@@ -143,24 +121,16 @@ public class RestApplication extends AbstractVerticle {
         .end(new Greeting(++counter, name).encode());
     });
 
-    // serve the dynamic config so the web client
-    // can also connect to the SSO server
     router.get("/keycloak.json").handler(ctx ->
       ctx.response()
         .putHeader(CONTENT_TYPE, "application/json; charset=utf-8")
         .end(keycloakJson.encode()));
 
-    // serve static files (web client)
     router.get().handler(StaticHandler.create());
 
-    // Create the HTTP server and pass the "accept" method to the request handler.
     vertx
       .createHttpServer()
       .requestHandler(router::accept)
-      .listen(
-        // Retrieve the port from the configuration,
-        // default to 8080.
-        config().getInteger("http.port", 8080),
-        ar -> done.handle(ar.mapEmpty()));
+      .listen(config().getInteger("http.port", 8080), ar -> done.handle(ar.mapEmpty()));
   }
 }
