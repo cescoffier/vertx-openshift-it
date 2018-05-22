407b6cb62288eea33dc4b208ee6724ae7f785b2e
Martin Spisiak <mspisiak@redhat.com>
2018-05-22 13:56:55 +0200

    Add initial classes for sockjs-service-proxy ITs
    - Add basic service
    - Add basic service implementation
    - Add main verticle
    - Add package-info for generating a service proxy and a proxy handler

---
+++ sockjs-service-proxy/src/main/java/io/vertx/openshift/proxy/ServerVerticle.java
@@ -0,0 +1,36 @@
+package io.vertx.openshift.proxy;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.bridge.PermittedOptions;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+import io.vertx.openshift.proxy.impl.MyServiceImpl;
+import io.vertx.serviceproxy.ServiceBinder;
+
+/**
+ * @author Martin Spisiak (mspisiak@redhat.com) on 21/05/18.
+ */
+public class ServerVerticle extends AbstractVerticle {
+  public static void main(String[] args) {
+
+  }
+
+  @Override
+  public void start() throws Exception {
+    MyService myService = new MyServiceImpl();
+
+    ServiceBinder binder = new ServiceBinder(vertx);
+    binder.setAddress("my-service-address").register(MyService.class, myService);
+
+    Router router = Router.router(vertx);
+    BridgeOptions options = new BridgeOptions()
+      .addInboundPermitted(new PermittedOptions().setAddress("my-service-address"))
+      .addOutboundPermitted(new PermittedOptions().setAddress("my-service-address"));
+
+    SockJSHandler sockJSHandler = SockJSHandler.create(vertx).bridge(options);
+    router.route("/eventbus/*").handler(sockJSHandler);
+
+    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+  }
+}
---
+++ sockjs-service-proxy/src/main/java/io/vertx/openshift/proxy/package-info.java
@@ -0,0 +1,4 @@
+@ModuleGen(groupPackage = "io.vertx.openshift.proxy", name = "vertx-sockjs-proxy")
+package io.vertx.openshift.proxy;
+
+import io.vertx.codegen.annotations.ModuleGen;
